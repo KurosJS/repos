438bd103cda7777df4ba5008d7a0e4e009b50b54
Kuros <93669487+KurosJS@users.noreply.github.com>
2022-11-06 14:23:02 +0300

    Update settings.py

--- home/box/web/ask/ask/settings.py
+++ home/box/web/ask/ask/settings.py
@@ -56,12 +56,14 @@ WSGI_APPLICATION = "ask.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
+    'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'stepic_web',
-        'USER': 'box'
+        'NAME': 'stepik_web',
+        'USER': 'box',
+        'HOST': 'localhost',
+        'PORT': '',
     }
-}
+} 
 
 
 # Password validation
